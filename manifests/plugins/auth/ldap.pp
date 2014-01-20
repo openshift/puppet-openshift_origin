e7e053a8db6b532c672989668411518d5b946500
stuart.warren <stuartwarren@ntlworld.com>
2014-01-20 22:57:25 +0000

    Reenable LDAP auth plugin
    LDAP seems to have been broken for a while as the manifest wasn't
    even included.
    This reenables the plugin, requires the installation of mod_ldap, and
    allows specifying credentials to bind to non anonymous access LDAP
    directories.

--- manifests/plugins/auth/ldap.pp
+++ manifests/plugins/auth/ldap.pp
@@ -1,12 +1,12 @@
 # Copyright 2013 Mojo Lingo LLC.
 # Modifications by Red Hat, Inc.
-# 
+#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,13 +17,13 @@ class openshift_origin::plugins::auth::ldap {
   if $::openshift_origin::broker_ldap_uri == '' {
     fail 'No LDAP URI specified (see ldap_uri).'
   }
-  
-  ensure_resource('package', 'rubygem-openshift-origin-auth-remote-user', {
+
+  ensure_resource('package', ['rubygem-openshift-origin-auth-remote-user','mod_ldap'], {
       ensure  => present,
       require => Class['openshift_origin::install_method'],
     }
   )
-  
+
   file { 'Broker httpd config':
     path    => '/var/www/openshift/broker/httpd/conf.d/openshift-origin-auth-remote-user-ldap.conf',
     content => template('openshift_origin/broker/plugins/auth/ldap/openshift-origin-auth-remote-user-ldap.conf.erb'),
@@ -36,7 +36,7 @@ class openshift_origin::plugins::auth::ldap {
     notify  => Service['openshift-broker'],
     before  => Exec['Broker gem dependencies'],
   }
-  
+
   file { 'Auth plugin config':
     path    => '/etc/openshift/plugins.d/openshift-origin-auth-remote-user.conf',
     content => template('openshift_origin/broker/plugins/auth/basic/remote-user.conf.plugin.erb'),
@@ -49,7 +49,7 @@ class openshift_origin::plugins::auth::ldap {
     notify  => Service['openshift-broker'],
     before  => Exec['Broker gem dependencies'],
   }
-  
+
   file { 'Console httpd config':
     path    => '/var/www/openshift/console/httpd/conf.d/openshift-origin-auth-remote-user-ldap.conf',
     content => template('openshift_origin/console/plugins/auth/ldap/openshift-origin-auth-remote-user-ldap.conf.erb'),
